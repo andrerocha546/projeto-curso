62c63b3f34c7d84a0065b4069d6c7d079adb24f9
André Rocha <andrerocha546@gmail.com>
2021-01-12 17:07:37 -0300

    Criando projeto

---
+++ src/main/java/com/andrerocha/clinic/ProjetoCurso2Application.java
@@ -0,0 +1,13 @@
+package com.andrerocha.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCurso2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCurso2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrerocha/clinic/ProjetoCurso2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrerocha.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCurso2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
