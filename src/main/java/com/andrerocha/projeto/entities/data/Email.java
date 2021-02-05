d81b8866218f1bd2eafa851fbfdd942fdb92dcca
André Rocha <andrerocha546@gmail.com>
2021-02-05 14:33:07 -0300

    Separando responsibilidades

--- src/main/java/com/andrerocha/projeto/entities/data/Email.java
+++ src/main/java/com/andrerocha/projeto/entities/data/Email.java
@@ -1,11 +1,10 @@
 package com.andrerocha.projeto.entities.data;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import com.andrerocha.projeto.entities.data.exception.DataException;
 
 public class Email {
+	private static final ValidatorEmail validator = new ValidatorEmail();
+	
 
 	private String email;
 
@@ -21,21 +20,8 @@ public class Email {
 		this.email = email;
 	}
 	
-	private static boolean isValidEmailAddressRegex(String email) {
-	    boolean isEmailIdValid = false;
-	    if (email != null && email.length() > 0) {
-	        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
-	        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
-	        Matcher matcher = pattern.matcher(email);
-	        if (matcher.matches()) {
-	            isEmailIdValid = true;
-	        }
-	    }
-	    return isEmailIdValid;
-	}
-	
 	private String isValidEmail(String email) {
-		if (!isValidEmailAddressRegex(email) || email == null) {
+		if (!validator.isValidEmailAddressRegex(email) || email == null) {
 			throw new DataException("Email " + email + " não é valido");
 		}
 		return email;
