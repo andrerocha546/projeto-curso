e96a70bfca73529b7403c477c517bf6be3ff97b5
André Rocha <andrerocha546@gmail.com>
2021-02-08 16:20:05 -0300

    Adicionando anotações

--- src/main/java/com/andrerocha/projeto/repositories/UserRepository.java
+++ src/main/java/com/andrerocha/projeto/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.andrerocha.projeto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.andrerocha.projeto.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
--- src/main/java/com/andrerocha/projeto/services/UserService.java
+++ src/main/java/com/andrerocha/projeto/services/UserService.java
@@ -3,10 +3,12 @@ package com.andrerocha.projeto.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.andrerocha.projeto.entities.User;
 import com.andrerocha.projeto.repositories.UserRepository;
 
+@Service
 public class UserService {
 
 	@Autowired
