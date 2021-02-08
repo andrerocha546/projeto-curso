75c91a978845d9373b1f2a9ad45501f615248762
André Rocha <andrerocha546@gmail.com>
2021-02-08 17:39:30 -0300

    Usando entity manager para fazer a instanciação dos objetos no DB

--- src/main/java/com/andrerocha/projeto/config/Instantiation.java
+++ src/main/java/com/andrerocha/projeto/config/Instantiation.java
@@ -1,40 +1,41 @@
 package com.andrerocha.projeto.config;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.andrerocha.projeto.builders.AdminBuilder;
 import com.andrerocha.projeto.builders.UserBuilder;
+import com.andrerocha.projeto.entities.Admin;
 import com.andrerocha.projeto.entities.User;
-import com.andrerocha.projeto.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
-public class Instantiation implements CommandLineRunner {
-	
-	@Autowired
-	private UserRepository userRepository;
+public class Instantiation {
 
-	@Override
-	public void run(String... args) throws Exception {
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("projetocurso");
+	private static EntityManager em = emf.createEntityManager();
+	
+	public static void main(String[] args) {
 		
 		User user = new UserBuilder()
 				.setName("Andre Rocha")
 				.setEmail("andrerocha546@gmail.com")
 				.setPassword("Asdsr11941918")
 				.getResult();
-		User user2 = new UserBuilder()
+		Admin admin = new AdminBuilder()
 				.setName("Andre Rocha")
 				.setEmail("andrerocha546@gmail.com")
 				.setPassword("Asdsr11941918")
 				.getResult();
 		
-		userRepository.saveAll(Arrays.asList(user, user2));
-		
+		em.getTransaction().begin();
+		em.persist(user);
+		em.persist(admin);
+		em.getTransaction().commit();
 	}
-	
-	
+
 }
