1abd6cc98692d8df5cea9ec360672c659cdd078e
André Rocha <andrerocha546@gmail.com>
2021-02-08 16:21:39 -0300

    Deletando arquivos relacionados a abstract class Person, que não existe mais

--- src/main/java/com/andrerocha/projeto/repositories/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.andrerocha.projeto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.andrerocha.projeto.entities.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Integer>{
-
-}
