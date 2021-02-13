708c1b5e0f4a98b644869e4dee7a6eb5c591e1c1
André Rocha <andrerocha546@gmail.com>
2021-02-13 16:04:07 -0300

    Apagando metodo ainda não implementado/usado

--- src/main/java/com/andrerocha/projeto/services/UserService.java
+++ src/main/java/com/andrerocha/projeto/services/UserService.java
@@ -1,8 +1,5 @@
 package com.andrerocha.projeto.services;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andrerocha.projeto.entities.User;
@@ -15,10 +12,6 @@ public class UserService {
 
 	public User insert(User user) {
 		return repository.register(user);
-	}
-
-	public List<User> findAll() {
-		return repository.findAll();
 	}
 
 }
