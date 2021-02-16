a07b6cc18dac7bb01a282d4c6f3386a6fb119190
André Rocha <andrerocha546@gmail.com>
2021-02-15 22:23:16 -0300

    Adicionando anotações Entity e Id a classe Article

--- src/main/java/com/andrerocha/projeto/entities/Article.java
+++ src/main/java/com/andrerocha/projeto/entities/Article.java
@@ -1,11 +1,18 @@
 package com.andrerocha.projeto.entities;
 
 import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import com.andrerocha.projeto.entities.data.article.ArticleData;
 
+@Entity
 public class Article {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Embedded
