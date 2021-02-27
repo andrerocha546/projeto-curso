298c4df4dc39352d0cb7cf88d6fca90c5efab3e4
André Rocha <andrerocha546@gmail.com>
2021-02-27 17:16:20 -0300

    feat: buscando Person's por ID

--- src/main/java/com/andrerocha/projeto/services/PersonService.java
+++ src/main/java/com/andrerocha/projeto/services/PersonService.java
@@ -1,12 +1,14 @@
 package com.andrerocha.projeto.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andrerocha.projeto.entities.Person;
 import com.andrerocha.projeto.repositories.PersonRepository;
+import com.andrerocha.projeto.services.exception.ObjectNotFoundException;
 
 @Service
 public class PersonService {
@@ -20,6 +22,11 @@ public class PersonService {
 	
 	public List<Person> findAll() {
 		return repository.findAll();
+	}
+	
+	public Person findById(Integer id) {
+		Optional<Person> person = repository.findById(id);
+		return person.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 
 }
