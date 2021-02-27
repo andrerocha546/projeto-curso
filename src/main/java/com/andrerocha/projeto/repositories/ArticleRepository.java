1657f94e1f6b8bf7e2b1fd787299e895292135f8
André Rocha <andrerocha546@gmail.com>
2021-02-27 16:19:59 -0300

    fix: adicionando anotações que identificam Repositories

--- src/main/java/com/andrerocha/projeto/repositories/ArticleRepository.java
+++ src/main/java/com/andrerocha/projeto/repositories/ArticleRepository.java
@@ -1,9 +1,11 @@
 package com.andrerocha.projeto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.andrerocha.projeto.entities.Article;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 
 }
--- src/main/java/com/andrerocha/projeto/repositories/CategoryRepository.java
+++ src/main/java/com/andrerocha/projeto/repositories/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.andrerocha.projeto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.andrerocha.projeto.entities.Category;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
 }
